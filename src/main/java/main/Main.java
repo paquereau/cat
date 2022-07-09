07bf0b69aa4bbed5007242475d63f9a0d4bf393a
paquereau <paquereau.fab@gmail.com>
2022-07-09 11:41:56 +0200

    Ajout main

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * The type Main.
+ */
+public class Main {
+
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Toto");
+    }
+}
